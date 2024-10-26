7fc0fe1d4893a24dfc1bcee76dd92a6da6567c49
Valeriia <valeriia.chyrko@oa.edu.ua>
2024-10-26 16:49:52 +0300

    Added operations of reading one and all records for Attempts entities.

---
+++ HomeAssignment.Persistence/Queries/Attempts/GetAttemptByIdQuery.cs
@@ -0,0 +1,6 @@
+﻿using HomeAssignment.DTOs.RespondDTOs;
+using MediatR;
+
+namespace HomeAssignment.Persistence.Queries.Attempts;
+
+public record GetAttemptByIdQuery(Guid Id) : IRequest<RespondAssignmentDto?>;
--- HomeAssignment.Persistence/Queries/Attempts/GetAttemptByIdQueryHandler.cs
+++ HomeAssignment.Persistence/Queries/Attempts/GetAttemptByIdQueryHandler.cs
@@ -4,26 +4,26 @@ using HomeAssignment.DTOs.RespondDTOs;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
-namespace HomeAssignment.Persistence.Queries.Assignments;
+namespace HomeAssignment.Persistence.Queries.Attempts;
 
-public sealed class GetAssignmentByIdQueryHandler : IRequestHandler<GetAssignmentByIdQuery, RespondAssignmentDto?>
+public sealed class GetAttemptByIdQueryHandler : IRequestHandler<GetAttemptByIdQuery, RespondAssignmentDto?>
 {
     private readonly IHomeworkAssignmentDbContext _context;
     private readonly IMapper _mapper;
 
-    public GetAssignmentByIdQueryHandler(IHomeworkAssignmentDbContext context, IMapper mapper)
+    public GetAttemptByIdQueryHandler(IHomeworkAssignmentDbContext context, IMapper mapper)
     {
         _context = context ?? throw new ArgumentNullException(nameof(context));
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
     }
 
-    public async Task<RespondAssignmentDto?> Handle(GetAssignmentByIdQuery query, CancellationToken cancellationToken)
+    public async Task<RespondAssignmentDto?> Handle(GetAttemptByIdQuery query, CancellationToken cancellationToken)
     {
-        var assignment = await _context
-            .AssignmentEntities
+        var attempt = await _context
+            .AttemptEntities
             .AsNoTracking()
             .FirstOrDefaultAsync(mr => mr.Id == query.Id, cancellationToken);
 
-        return assignment != null ? _mapper.Map<RespondAssignmentDto>(assignment) : null;
+        return attempt != null ? _mapper.Map<RespondAssignmentDto>(attempt) : null;
     }
 }
