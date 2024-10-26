5cd85bb9dddab5011c5fb89e97fb3c986a4c32c4
Valeriia <valeriia.chyrko@oa.edu.ua>
2024-10-26 15:49:35 +0300

    CUD for Student entities have been implemented.

---
+++ HomeAssignment.Persistence/Commands/Students/DeleteStudentCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using HomeAssignment.Database.Contexts.Abstractions;
+using MediatR;
+
+namespace HomeAssignment.Persistence.Commands.Students;
+
+public sealed class DeleteStudentCommandHandler : IRequestHandler<DeleteStudentCommand>
+{
+    private readonly IHomeworkAssignmentDbContext _context;
+
+    public DeleteStudentCommandHandler(IHomeworkAssignmentDbContext context)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task Handle(DeleteStudentCommand command, CancellationToken cancellationToken)
+    {
+        if (command is null)
+        {
+            throw new ArgumentNullException(nameof(command));
+        }
+
+        var userEntity = await _context.UserEntities.FindAsync(command.UserId, cancellationToken);
+        if (userEntity != null)
+        {
+            _context.UserEntities.Remove(userEntity);
+        }
+    }
+}
---
+++ HomeAssignment.Persistence/Commands/Students/UpdateStudentCommandHandler.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using HomeAssignment.Database.Contexts.Abstractions;
+using HomeAssignment.Database.Entities;
+using HomeAssignment.Domain.Abstractions;
+using HomeAssignment.DTOs.RespondDTOs;
+using MediatR;
+
+namespace HomeAssignment.Persistence.Commands.Students;
+
+public sealed record UpdateStudentCommandHandler : IRequestHandler<UpdateStudentCommand, RespondStudentDto>
+{
+    private readonly IHomeworkAssignmentDbContext _context;
+    private readonly IMapper _mapper;
+
+    public UpdateStudentCommandHandler(IHomeworkAssignmentDbContext context, IMapper mapper)
+    {
+        _context = context ?? throw new ArgumentNullException(nameof(context));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+    }
+
+    public Task<RespondStudentDto> Handle(UpdateStudentCommand command,
+        CancellationToken cancellationToken = default)
+    {
+        if (command is null)
+        {
+            throw new ArgumentNullException(nameof(command));
+        }
+        
+        var student = Student.Create(
+            command.StudentDto.FullName,
+            command.StudentDto.Email,
+            command.StudentDto.Password,
+            command.StudentDto.GithubUsername,
+            command.StudentDto.GithubAccessToken,
+            command.StudentDto.GithubProfileUrl,
+            command.StudentDto.GithubPictureUrl
+        );
+
+        student.UserId = command.UserId;
+        var userEntity = _mapper.Map<UserEntity>(student); 
+        _context.UserEntities.Update(userEntity);
+
+        student.GitHubProfileId = command.GitHubProfileId;
+        var profileEntity = _mapper.Map<GitHubProfilesEntity>(student);
+        _context.GitHubProfilesEntities.Update(profileEntity);
+
+        return Task.FromResult(_mapper.Map<RespondStudentDto>(student));
+    }
+}
