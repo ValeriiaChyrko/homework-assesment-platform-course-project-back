abb171d7caa3b32612ec76d49ebdb4b3d7f6a398
Valeriia <valeriia.chyrko@oa.edu.ua>
2024-10-27 13:47:19 +0200

    Basic api methods for AssignmentController have been created.

---
+++ HomeworkAssignment.API/Controllers/AssignmentController.cs
@@ -0,0 +1,71 @@
+﻿using HomeAssignment.DTOs.RequestDTOs;
+using HomeAssignment.DTOs.RespondDTOs;
+using HomeworkAssignment.Application.Abstractions.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HomeworkAssignment.API.Controllers;
+
+[Route("api/assignments")]
+[Produces("application/json")]
+[ApiController]
+public class AssignmentController : ControllerBase
+{
+    private readonly IAssignmentService _assignmentService;
+
+    public AssignmentController(IAssignmentService assignmentService)
+    {
+        _assignmentService = assignmentService;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<IReadOnlyList<RespondAssignmentDto>>> Get()
+    {
+        var result = await _assignmentService.GetAssignmentsAsync();
+        return StatusCode(StatusCodes.Status200OK, result);
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<RespondAssignmentDto>> Get(Guid id)
+    {
+        var result = await _assignmentService.GetAssignmentByIdAsync(id);
+        return StatusCode(StatusCodes.Status200OK, result);
+    }
+    
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<Guid>> Create([FromBody] RequestAssignmentDto request)
+    {
+        var result = await _assignmentService.CreateAssignmentAsync(request);
+        return StatusCode(StatusCodes.Status201Created, result);
+    }
+
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult<Guid>> Delete(Guid id)
+    {
+        await _assignmentService.DeleteAssignmentAsync(id);
+        return StatusCode(StatusCodes.Status200OK, id);
+    }
+
+    [HttpPut("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<RespondAssignmentDto>> Update(Guid id, RequestAssignmentDto request)
+    {
+        var response = await _assignmentService.UpdateAssignmentAsync(id, request);
+        return StatusCode(StatusCodes.Status200OK, response);
+    }
+}
---
+++ HomeworkAssignment.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using HomeworkAssignment.Application.Abstractions.Contracts;
+using HomeworkAssignment.Application.Implementations;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HomeworkAssignment.Application;
+
+public static class DependencyInjection
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IAssignmentService, AssignmentService>();
+        services.AddSingleton<IAttemptService, AttemptService>();
+        services.AddSingleton<IStudentService, StudentService>();
+        services.AddSingleton<ITeacherService, TeacherService>();
+
+        services.AddScoped<IDatabaseTransactionManager, DatabaseTransactionManager>();
+        services.AddSingleton<IPasswordHasher, PasswordHasher>();
+    }
+}
